a0c51240e4486c4efc6cd6826536bd908117b319
Behnam S.A <behnamsa96@outlook.com>
2023-03-01 03:54:49 +0330

    Implement "Typography" of "HeroBanner" component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
-  )
-}
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fitness club
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px", xs: "40px" },
+        }}
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px">
+        Check out the most effective exersices
+      </Typography>
+    </Box>
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
