b71445abd70aa11a4d4353bb7d203245cf8120b8
Behnam S.A <behnamsa96@outlook.com>
2023-03-01 03:44:39 +0330

    Create components of home page
    - Create "HeroBanner" component
    - Create "SearchExercises" component
    - Create "Exercises" component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
