2612aa2bf07deb1d52f9d57ee7c7c69a7187c05b
Behnam S.A <behnamsa96@outlook.com>
2023-03-04 18:26:09 +0330

    Implement "Exercises" component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
+
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "110px" }, mt: "50px", p: "20px" }}>
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <p key={index}>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
-export default Exercises
+export default Exercises;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,9 +18,9 @@ const Home = () => {
         setBodyPart={setBodyPart}
       />
       <Exercises
+        exercises={exercises}
         setExercises={setExercises}
         bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
       />
     </Box>
   );
