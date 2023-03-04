93ba3b8adc98186d58e2014843da0475a63d262b
Behnam S.A <behnamsa96@outlook.com>
2023-03-04 18:55:12 +0330

    Implement "ExerciseCard" component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,6 +3,7 @@ import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   console.log(exercises);
@@ -19,7 +20,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         justifyContent="center"
       >
         {exercises.map((exercise, index) => (
-          <p key={index}>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
     </Box>
