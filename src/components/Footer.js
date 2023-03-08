c96b29a623b4a4ed408e3061526333fbb6297ee6
Behnam S.A <behnamsa96@outlook.com>
2023-03-08 10:10:08 +0330

    Implement "Footer" component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="20px">
+          Made by Behnam SeydAbadi
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
