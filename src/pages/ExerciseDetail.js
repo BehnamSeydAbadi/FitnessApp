27185ae2f136867f658227f76513cda9c0e6aba1
Behnam S.A <behnamsa96@outlook.com>
2023-03-08 07:32:41 +0330

    Implement fetching exercise detail from rapidApi
    - Add "Detail" component
    - Add "ExerciseVideos" component
    - Add "SimilarExercises" component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
-}
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
 
-export default ExerciseDetail
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+
+      setExerciseDetail(exerciseDetailData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
